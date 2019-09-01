d5e3bf866fbba501477574319f0266aacc61fa7c
Ravindu <ravindugunarathna@gmail.com>
2019-09-02 01:02:37 +0530

    Add Changes

--- src/Hibernate/IMPL/CourseHIBimpl.java
+++ src/Hibernate/IMPL/CourseHIBimpl.java
@@ -1,7 +1,34 @@
 package Hibernate.IMPL;
 
 import Hibernate.Custom.CourseHIB;
+import Hibernate.Entity.Course;
+
+import java.util.List;
 
 public class CourseHIBimpl implements CourseHIB {
 
+    @Override
+    public boolean add(Course dto) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean update(Course dto) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws Exception {
+        return false;
+    }
+
+    @Override
+    public Course search(String s) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Course> getAll() throws Exception {
+        return null;
+    }
 }
--- src/Hibernate/IMPL/StudentHIBimpl.java
+++ src/Hibernate/IMPL/StudentHIBimpl.java
@@ -1,6 +1,59 @@
 package Hibernate.IMPL;
 
 import Hibernate.Custom.StudentHIB;
+import Hibernate.Dao.HIBCUDutill;
+import Hibernate.Entity.Student;
+
+import java.io.Serializable;
+import java.util.List;
 
 public class StudentHIBimpl implements StudentHIB {
+    @Override
+    public boolean add(Student dto) throws Exception {
+        HIBCUDutill.openSession();
+        Serializable id=HIBCUDutill.session.save(dto);
+        if(id!=null){
+            HIBCUDutill.closeSession();
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Student dto) throws Exception {
+        HIBCUDutill.openSession();
+        Student student=HIBCUDutill.session.get(Student.class,dto.getRegNumber());
+        student.setName(dto.getName());
+        student.setDate(dto.getDate());
+        student.setEmail(dto.getEmail());
+        student.setTelNumber(dto.getTelNumber());
+        student.setEmail(dto.getEmail());
+        student.setTradeOne(dto.getTradeOne());
+        student.setTradeTwo(dto.getTradeTwo());
+        student.setTradeThree(dto.getTradeThree());
+        student.setNicNumber(dto.getNicNumber());
+        student.setTrainingType(dto.getTrainingType());
+        if(student!=null){
+            HIBCUDutill.closeSession();
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(String s) throws Exception {
+        return false;
+    }
+
+    @Override
+    public Student search(String s) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Student> getAll() throws Exception {
+        return null;
+    }
 }
